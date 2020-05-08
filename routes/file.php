d3f24466d2575f39abd15769e89742629258a958
bmatheus <bmatheus@sellmark.net>
2020-05-08 10:40:21 -0500

    Added routes to stream/download files from s3 bucket

---
+++ routes/file.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| File Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function(){
+	abort(404);
+});
+Route::get('download/{key?}', 'FileController@download')->where('key', '(.*)');
+Route::get('{key?}', 'FileController@stream')->where('key', '(.*)');
+
+
+
